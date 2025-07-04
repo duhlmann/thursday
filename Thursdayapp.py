24a4f7db42c6e6f5f4ed9b45d9cbd007b0ab8360
Dominica Uhlmann <duhlmann@users.noreply.github.com>
2025-07-03 17:36:22 -0700

    Update Thursdayapp.py

--- Thursdayapp.py
+++ Thursdayapp.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-streamclean = pd.read_csv("Thursday.csv", header=0, index_col=0)
+column_names = ['Index', 'Dates', 'Day', 'Net Consumption (kWh)', 'Extra1', 'Extra2', 'Extra3']
 
-st.write("Columns found in CSV:", list(streamclean.columns))
+streamclean = pd.read_csv("Thursday.csv", header=None, names=column_names)
+
+# Drop 'Index' and any 'Extra' columns (empty columns)
+streamclean = streamclean.drop(columns=['Index', 'Extra1', 'Extra2', 'Extra3'])
+
+st.write("Columns found in CSV after renaming and dropping:", list(streamclean.columns))
 st.write(streamclean.head())
-
