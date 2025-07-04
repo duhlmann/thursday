d8cc1c88113aa9ec345ba957155600449d6702b1
Dominica Uhlmann <duhlmann@users.noreply.github.com>
2025-07-03 17:23:44 -0700

    Update Thursdayapp.py

--- Thursdayapp.py
+++ Thursdayapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-streamclean = pd.read_csv("Thursday.csv")
+streamclean = pd.read_csv("Thursday.csv", header=0)
 
 # Show column names in the app so we know what to use
 st.write("Columns in CSV:", list(streamclean.columns))
