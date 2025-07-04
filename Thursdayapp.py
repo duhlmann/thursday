c1c618343f392bfa540e664319ba84c21b7a137d
Dominica Uhlmann <duhlmann@users.noreply.github.com>
2025-07-03 17:34:05 -0700

    Update Thursdayapp.py

--- Thursdayapp.py
+++ Thursdayapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-streamclean = pd.read_csv("Thursday.csv")
+streamclean = pd.read_csv("Thursday.csv", header=0)
 
 st.write("Columns found in CSV:", list(streamclean.columns))
