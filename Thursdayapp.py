f7353884a02f2da9206bf43e59badc556b07097e
Dominica Uhlmann <duhlmann@users.noreply.github.com>
2025-07-03 17:29:11 -0700

    Update Thursdayapp.py

--- Thursdayapp.py
+++ Thursdayapp.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-
-streamclean = pd.read_csv("Thursday.csv", header=None)
+streamclean = pd.read_csv("Thursday.csv", header=None).dropna(axis=1, how='all')
 st.write(streamclean.head())
 
 # Show column names in the app so we know what to use
